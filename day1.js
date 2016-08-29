a5e70e30cd14021b5d2261a78bdbfc138c11b677
Dreamer999 <eternal08135566@gmail.com>
2016-08-29 22:30:45 +0800

    happy

--- day1.js
+++ day1.js
@@ -1,15 +1,15 @@
 var http = require('http')
 var options = {
   hostname: 'd8688bca.ngrok.io',
-  port: 80,
+  port: 3000,
   path: '/',
-  method: 'POST',
+  method: 'GET',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/x-www-from-urlencoded'
   }
 };
 
-
+console.log('start');
 // http.get("http://64cae4dc.ngrok.io",function()
 // {
 //         console.log("check");
@@ -25,18 +25,12 @@ serialPort.on("open", function () {
   serialPort.on('data', function(data) {
     // Receive data from Arduino chip (32U4)
     var req = http.request(options, function(res){
-  	res.setEncoding('utf8');
-  	res.on('data', function(chunk){
- 
-  	});
-  	res.on('end', function(){
-   	 console.log('No more data in response.');
- 	 });
+  	
 	});
 	req.on('error',function(e){
 	});
 	console.log(JSON.stringify(data));
-    req.write(JSON.stringify(data));
+    req.write("JSON");
 	req.end();
   });
 });
